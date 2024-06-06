2bb22558130c8fc76bae58eb8304157876f36565
Keshav Madhav <keshav2552003@gmail.com>
2024-06-06 21:23:41 +0530

    Completed card

--- docs_mini/src/components/Card.jsx
+++ docs_mini/src/components/Card.jsx
@@ -1,7 +1,43 @@
-const Card = () => {
+import { FaRegFileAlt } from 'react-icons/fa'
+import { IoClose } from 'react-icons/io5'
+import { LuDownload } from 'react-icons/lu'
+
+const Card = ({
+  title,
+  size,
+  description,
+  tagDetails: { isOpen, tagTitle, tagColor },
+  download,
+}) => {
   return (
-    <div className="w-60 h-72 rounded-[20px] bg-zinc-900/90 ">
-      
+    <div className="relative flex-shrink-0 w-60 h-72 rounded-[40px] bg-zinc-900/90 text-white py-8 px-6 overflow-hidden">
+      <div className='flex items-center gap-2'>
+        <FaRegFileAlt />
+        <h3 className='text-md font-semibold'>{title}</h3>
+      </div>
+      <p className='text-sm mt-3 font-semibold leading-tight'>{description}</p>
+
+      <div className={`absolute bottom-0 w-full left-0 ${!isOpen && 'bottom-2'}`}>
+        <div className='flex items-center justify-between py-3 px-7'>
+          <h5>{size}</h5>
+          {download? (
+            <div title='Download' className='w-6 h-6 flex items-center justify-center bg-zinc-500 text-white rounded-full cursor-pointer'>
+              <LuDownload className='h-3 w-3'/>
+            </div>
+          ):(
+            <div title="Cancel" className='w-6 h-6 flex items-center justify-center bg-zinc-500 text-white rounded-full cursor-pointer'>
+              <IoClose className='h-4 w-4'/>
+            </div>
+          )}
+        </div>
+
+        {isOpen && (
+          <div className='w-full h-10 flex items-center justify-center' style={{backgroundColor: tagColor}}>
+            <h3 className='text-sm font-semibold'>{tagTitle}</h3>
+          </div>
+        )}
+
+      </div>
     </div>
   )
 }
--- docs_mini/src/components/Foreground.jsx
+++ docs_mini/src/components/Foreground.jsx
@@ -1,9 +1,76 @@
 import Card from "./Card"
+import { useState } from "react"
 
 function Foreground() {
+  const [data, setData] = useState([
+    {
+      title: "File 1",
+      size: "0.4mb",
+      description: "Lorem ipsum dolor sit, amet consectetur adipisicing elit.",
+      tagDetails: {
+        isOpen: false,
+        tagTitle: "Download Now",
+        tagColor: "rgb(22, 163, 74)",
+      },
+      download: false,
+    },
+    {
+      title: "File 2",
+      size: "0.3mb",
+      description: "Lorem ipsum dolor sit, amet consectetur adipisicing elit.",
+      tagDetails: {
+        isOpen: true,
+        tagTitle: "Download Now",
+        tagColor: "rgb(22, 163, 74)",
+      },
+      download: true,
+    },
+    {
+      title: "File 3",
+      size: "0.5mb",
+      description: "Lorem ipsum dolor sit, amet consectetur adipisicing elit.",
+      tagDetails: {
+        isOpen: false,
+        tagTitle: "Upload",
+        tagColor: "rgb(37, 99, 235)",
+      },
+      download: false,
+    },
+    {
+      title: "File 4",
+      size: "0.6mb",
+      description: "Lorem ipsum dolor sit, amet consectetur adipisicing elit.",
+      tagDetails: {
+        isOpen: false,
+        tagTitle: "Upload",
+        tagColor: "rgb(37, 99, 235)",
+      },
+      download: false,
+    },
+    {
+      title: "File 5",
+      size: "0.7mb",
+      description: "Lorem ipsum dolor sit, amet consectetur adipisicing elit.",
+      tagDetails: {
+        isOpen: true,
+        tagTitle: "Upload",
+        tagColor: "rgb(37, 99, 235)",
+      },
+      download: true,
+    },
+  ])
   return (
-    <div className="fixed x-[3] w-full h-full top-0 left-0 ">
-      <Card />
+    <div className="fixed z-[3] w-full h-full top-0 left-0 flex gap-6 flex-wrap p-5">
+      {data.map((item, index) => (
+        <Card
+          key={index}
+          title={item.title}
+          size={item.size}
+          description={item.description}
+          tagDetails={item.tagDetails}
+          download={item.download}
+        />
+      ))}
     </div>
   )
 }
