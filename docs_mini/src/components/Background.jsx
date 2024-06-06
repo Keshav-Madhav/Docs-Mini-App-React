6d9a8472ac1f82f44600826ba70f955a1549f84d
Keshav Madhav <keshav2552003@gmail.com>
2024-06-06 21:55:30 +0530

    Added drag to add text file

--- docs_mini/src/components/Background.jsx
+++ docs_mini/src/components/Background.jsx
@@ -1,10 +1,20 @@
+
+
 const Background = () => {
+  
+
   return (
-    <div className="fixed w-full h-screen z-[2]">
+    <div 
+      
+      className="fixed w-full h-screen z-[2]"
+    >
+     
       <div className="absolute top-[5%] w-full py-8 flex justify-center text-zinc-500 font-semibold text-xl">
         Documents.
       </div>
-      <h1 className="absolute top-1/2 left-1/2 -translate-x-[50%] -translate-y-[50%] text-[15vw] leading-none tracking-tighter font-semibold text-zinc-900">Docs.</h1>
+      <h1 className="absolute top-1/2 left-1/2 -translate-x-[50%] -translate-y-[50%] text-[15vw] leading-none tracking-tighter font-semibold text-[rgb(232,145,47)]">
+        Docs.
+      </h1>
     </div>
   )
 }
--- docs_mini/src/components/Foreground.jsx
+++ docs_mini/src/components/Foreground.jsx
@@ -1,78 +1,83 @@
 import Card from "./Card"
-import { useRef, useState } from "react"
+import { useDropzone } from "react-dropzone"
+import { useEffect, useState, useRef } from "react"
 
 function Foreground() {
   const ref = useRef(null)
-  const [data, setData] = useState([
-    {
-      title: "File 1",
-      size: "0.4mb",
-      description: "Lorem ipsum dolor sit, amet consectetur adipisicing elit.",
-      tagDetails: {
-        isOpen: false,
-        tagTitle: "Download Now",
-        tagColor: "rgb(22, 163, 74)",
-      },
-      download: false,
+  const [data, setData] = useState([])
+
+  const [uploadedFiles, setUploadedFiles] = useState([])
+  const { getRootProps, getInputProps, isDragActive } = useDropzone({
+    noClick: true,
+    accept: { 'text/plain': ['.txt'] },
+    onDrop: (acceptedFiles) => {
+      if (acceptedFiles.length > 0) {
+        setUploadedFiles([...uploadedFiles, acceptedFiles[0]]);
+      } 
     },
-    {
-      title: "File 2",
-      size: "0.3mb",
-      description: "Lorem ipsum dolor sit, amet consectetur adipisicing elit.",
-      tagDetails: {
-        isOpen: true,
-        tagTitle: "Download Now",
-        tagColor: "rgb(22, 163, 74)",
-      },
-      download: true,
-    },
-    {
-      title: "File 3",
-      size: "0.5mb",
-      description: "Lorem ipsum dolor sit, amet consectetur adipisicing elit.",
-      tagDetails: {
-        isOpen: false,
-        tagTitle: "Upload",
-        tagColor: "rgb(37, 99, 235)",
-      },
-      download: false,
-    },
-    {
-      title: "File 4",
-      size: "0.6mb",
-      description: "Lorem ipsum dolor sit, amet consectetur adipisicing elit.",
-      tagDetails: {
-        isOpen: false,
-        tagTitle: "Upload",
-        tagColor: "rgb(37, 99, 235)",
-      },
-      download: false,
-    },
-    {
-      title: "File 5",
-      size: "0.7mb",
-      description: "Lorem ipsum dolor sit, amet consectetur adipisicing elit.",
-      tagDetails: {
-        isOpen: true,
-        tagTitle: "Upload",
-        tagColor: "rgb(37, 99, 235)",
-      },
-      download: true,
-    },
-  ])
+  });
+
+  useEffect(() => {
+    if (uploadedFiles.length > 0) {
+      uploadedFiles.forEach((file) => {
+        const reader = new FileReader();
+        reader.onload = (e) => {
+          const content = e.target.result;
+          const title = file.name.split(".")[0];
+          const size = file.size;
+          const description = content.slice(0, 100);
+          const tagDetails = {
+            isOpen: true,
+            tagTitle: "New",
+            tagColor: "rgb(232,145,47)", // rgb(37, 99, 235) rgb(22, 163, 74)
+          };
+          const download = true;
+          setData((prevData) => [
+            ...prevData,
+            { title, size, description, tagDetails, download },
+          ]);
+        };
+        reader.readAsText(file);
+      });
+    }
+
+    console.log(uploadedFiles);
+  }
+  , [uploadedFiles]);
+
   return (
-    <div ref={ref} className="fixed z-[3] w-full h-full top-0 left-0 flex gap-6 flex-wrap p-5">
-      {data.map((item, index) => (
-        <Card
-          key={index}
-          title={item.title}
-          size={item.size}
-          description={item.description}
-          tagDetails={item.tagDetails}
-          download={item.download}
-          reference={ref}
-        />
-      ))}
+    <div 
+      {...getRootProps()}
+      onKeyDown={(e) => {
+        if (e.key === "Enter") e.preventDefault();
+      }}
+      onClick={(e) => {
+        e.preventDefault();
+      }}
+      className="fixed z-[3] w-full h-full top-0 left-0"
+    >
+      <input
+        {...getInputProps()}
+        type="file"
+        name="UploadFiles"
+        id="UploadFiles"
+        tabIndex={-1}
+        className="hidden"
+      />
+      
+      <div ref={ref} className="fixed w-full h-full flex gap-6 flex-wrap p-5">
+        {data.map((item, index) => (
+          <Card
+            key={index}
+            title={item.title}
+            size={item.size}
+            description={item.description}
+            tagDetails={item.tagDetails}
+            download={item.download}
+            reference={ref}
+          />
+        ))}
+      </div>
     </div>
   )
 }
